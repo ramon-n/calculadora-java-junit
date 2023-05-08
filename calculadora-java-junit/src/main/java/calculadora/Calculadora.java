16607f2c63efaa6e332c96c68d8185a434cb284b
ramon pereira do nascimento <88009771+ramon-n@users.noreply.github.com>
2023-05-08 17:16:09 -0300

    Add files via upload

---
+++ calculadora-java-junit/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,58 @@
+package calculadora;
+import java.util.Scanner;
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Entre com o primeiro numero");
+		int num1 = input.nextInt();
+		System.out.println("Entre com o segundo numero");
+		int num2 = input.nextInt();
+		
+		
+		
+		int soma = calc.somar(num1, num2);
+		System.out.println("Resultado é: " + soma);
+
+		
+		int sub = calc.subtracao(num1, num2);
+		System.out.println("Resultado é: " + sub);
+
+		
+		int multi = calc.multiplicar(num1, num2);
+		System.out.println("Resultado é: " + multi);
+
+		
+		int divi = calc.dividir(num1, num2);
+		System.out.println("Resultado é: " + divi);
+
+		
+		double poten = calc.potencia(num1, num2);
+		System.out.println("Resultado é: " + poten);
+		input.close();
+	}
+
+	int somar(int a, int b) {
+		return a + b;
+	}
+
+	int subtracao(int a, int b) {
+
+		return a - b;
+	}
+
+	int multiplicar(int a, int b) {
+		return a * b;
+	}
+
+	int dividir(int a, int b) {
+		return a / b;
+	}
+
+	double potencia(int a, int b) {
+		return Math.pow(a, b);
+	}
+
+}
